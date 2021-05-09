dcf4ce8658a7df99daeddc6ba5217e497aebaaba
Vignesh Rao <vigneshravirao@gmail.com>
2021-05-09 20:54:51 +0530

    :sparkles: Update myColors

--- lib/globals/myColors.dart
+++ lib/globals/myColors.dart
@@ -4,3 +4,4 @@ const Color kGrey = Color.fromRGBO(117, 117, 117, 1);
 const Color kYellow = Color.fromRGBO(253, 216, 53, 1);
 const Color kWhite = Colors.white;
 const Color kBlack = Colors.black;
+const Color kBlue = Color.fromRGBO(61, 130, 255, 1);
