7cd42bc42117d36448d44076e4ae3e5948eb053d
Aishani Mitra <66082466+Aishani2001@users.noreply.github.com>
2021-06-12 18:37:28 +0530

    Update myListTile.dart

--- lib/widgets/myListTile.dart
+++ lib/widgets/myListTile.dart
@@ -1,13 +1,19 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:timetable_2_flutter_admin/globals/myColors.dart';
 import 'package:timetable_2_flutter_admin/globals/myFonts.dart';
 import 'package:timetable_2_flutter_admin/globals/mySpaces.dart';
+import 'package:timetable_2_flutter_admin/widgets/Pop_up_dialog.dart';
 import 'package:timetable_2_flutter_admin/globals/sizeConfig.dart';
-import 'package:timetable_2_flutter_admin/widgets/appBar.dart';
-import 'package:timetable_2_flutter_admin/widgets/popUp.dart';
 
+import '../globals/myColors.dart';
+import '../globals/myColors.dart';
+import '../globals/myColors.dart';
+import '../globals/myColors.dart';
+
+// ignore: must_be_immutable
 class MyListTile extends StatelessWidget {
   final String title;
   final String time;
@@ -26,110 +32,35 @@ class MyListTile extends StatelessWidget {
       this.time,
       this.duration,
       this.email}) {
-    switch (this.type) {
-      case 'Assignment':
+
+    switch (this.status) {
+      case 'cancel':
         sideColor = kBlack;
+        text = kRed;
+        bgColor=lRed;
         break;
-      case 'Quiz':
-        sideColor = kYellow;
+      case 'update':
+        sideColor = kBlue;
+        text = kYellow;
+        bgColor=lYellow;
         break;
       default:
         sideColor = kBlue;
-    }
-    switch (this.status) {
-      case 'cancel':
-        text = kRed;
-        bgColor = lRed;
-        break;
-      case 'update':
-        text = kYellow;
-        bgColor = lYellow;
-        break;
-      default:
-        bgColor = lBlue;
+        bgColor=lBlue;
         text = kBlue;
     }
   }
+
+
 
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return InkWell(
-      // When the user taps the button, show a dialog box.
-      onTap: () {
-        showModalBottomSheet(
-            barrierColor: Colors.transparent,
-            isScrollControlled: true,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
-            context: context,
-            builder: (context) {
-              return Container(
-                height: MediaQuery.of(context).size.height * 0.8,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    AppBar(
-                      leading: Icon(Icons.note_add_outlined),
-                      title: Text('$title'),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: MySpaces.horizontalScreenPadding),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Icon(Icons.school_outlined),
-                              Expanded(child: Text('Course Code')),
-                              Expanded(
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(5),
-                                      border: Border.all(
-                                        color: kBlue,
-                                      )),
-                                  child: Text('$title'),
-                                ),
-                              )
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Icon(Icons.tag),
-                              Expanded(child: Text('tags')),
-                              Expanded(
-                                child: Text('$type'),
-                              )
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Icon(Icons.person_pin),
-                              Expanded(child: Text('User')),
-                              Expanded(
-                                child: Text('$email'),
-                              )
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Icon(Icons.calendar_today_outlined),
-                              Expanded(child: Text('Date Added')),
-                              Expanded(
-                                child: Text("11th June"),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            });
-        // showPopup(context, _popupBody(), '$title $status');
-      },
+        // When the user taps the button, show a dialog box.
+        onTap: () {
+          showPopup(context, _popupBody( title, type , email), '$title $status');
+        },
       child: Container(
         child: Padding(
           padding: EdgeInsets.fromLTRB(MySpaces.listTileLeftPadding, 0, 0,
@@ -141,36 +72,15 @@ class MyListTile extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text(
-                            '$title',
-                            style: MyFonts.extraBold.size(25),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Container(
-                            // color: kBlack,
-                            decoration: BoxDecoration(
-                                color: bgColor,
-                                borderRadius: BorderRadius.circular(18.25)),
-                            // height: 20,
-                            // width: 79,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 5),
-                              child: Text(
-                                '$status',
-                                style: MyFonts.medium.setColor(text).size(10),
-                              ),
-                            ),
-                          ),
-                        ],
+                      child: Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Text(
+                          '$title',
+                          style: MyFonts.extraBold.size(25),
+                        ),
                       ),
                     ),
+
                     Expanded(
                       child: Row(
                         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -190,16 +100,39 @@ class MyListTile extends StatelessWidget {
                   ],
                 ),
               ),
+              Container(
+                decoration: BoxDecoration(
+                    color: bgColor,
+                  borderRadius: BorderRadius.circular(18.25)
+                ),
+                height: 20,
+                width: 79,
+
+                child: SizedBox(
+
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      '$status',
+                      style:
+                        MyFonts.medium.setColor(text).size(10),
+                    ),
+                  ),
+                ),
+              ),
               Expanded(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Expanded(
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Text(
-                          '$time',
-                          style: MyFonts.extraBold.size(20),
+                    Container(
+                      height: 20,
+                      child: Expanded(
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Text(
+                            '$time',
+                            style: MyFonts.extraBold.size(20),
+                          ),
                         ),
                       ),
                     ),
@@ -232,8 +165,8 @@ class MyListTile extends StatelessWidget {
           gradient:
               LinearGradient(stops: [0.05, 0.05], colors: [sideColor, kWhite]),
           color: kWhite,
-          border: Border.all(color: grey2, width: 1),
-          borderRadius: BorderRadius.circular(4),
+           border: Border.all(color: grey2, width: 1),
+           borderRadius: BorderRadius.circular(4),
         ),
       ),
     );
@@ -242,20 +175,22 @@ class MyListTile extends StatelessWidget {
 
 showPopup(BuildContext context, Widget widget, String title,
     {BuildContext popupContext}) {
+
   Navigator.push(
     context,
     PopupLayout(
-      top: 1,
-      left: 10,
-      right: 10,
-      bottom: 10,
+      top: 100,
+      left: 0,
+      right: 0,
+      bottom: 0,
       child: PopupContent(
         content: Scaffold(
+
           appBar: AppBar(
             title: Text(title),
             leading: new Builder(builder: (context) {
               return IconButton(
-                icon: Icon(Icons.arrow_back),
+                icon: Icon(Icons.note_add_outlined),
                 onPressed: () {
                   try {
                     Navigator.pop(context); //close the popup
@@ -265,6 +200,7 @@ showPopup(BuildContext context, Widget widget, String title,
             }),
             brightness: Brightness.light,
           ),
+
           body: widget,
         ),
       ),
@@ -272,6 +208,127 @@ showPopup(BuildContext context, Widget widget, String title,
   );
 }
 
-Widget _popupBody() {
-  return Container();
+Widget _popupBody(String title,String type,String email) {
+  return Container(
+    child: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(42.0),
+          child: Row(
+            children: [
+               Padding(
+                 padding: const EdgeInsets.fromLTRB(0.0,0.0,20,0.0),
+                 child: Icon(Icons.school_outlined),
+               ),
+              Expanded(
+                  child: Text('Course Code')),
+              Expanded(
+                child: Container(
+
+                  height: 50,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                        color: kBlue,
+                      )),
+                  child: Container(
+                      decoration: BoxDecoration(),
+                      padding: EdgeInsets.fromLTRB(30.0,10.0,.0,10),
+                      child: Text(
+
+                          '$title',
+                        style: MyFonts.medium.size(20),
+
+                      )
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(40.0,0.0,40.0,30.0),
+          child: Container(
+            height: 70,
+            width: 360,
+            decoration: BoxDecoration(
+              gradient:
+              LinearGradient(stops: [60,60.25], colors: [lBlue,lBlue]),
+              color: lBlue,
+              border: Border.all(color: grey2, width: 1),
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Class'),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(40.0,0.0,40.0,30.0),
+          child: Row(
+            children: [
+               Padding(
+                 padding: const EdgeInsets.fromLTRB(0.0,0.0,20,0.0),
+                 child: Icon(Icons.tag),
+               ),
+
+              Expanded(child: Text('tags')),
+              Expanded(
+                child: Text('$type'),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(40.0,0.0,40.0,30.0),
+          child: Row(
+            children: [
+               Padding(
+                 padding: const EdgeInsets.fromLTRB(0.0,0.0,20,0.0),
+                 child: Icon(Icons.person_pin),
+               ),
+
+              Expanded(child: Text('User')),
+              Expanded(
+                child: Text('$email'),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(40.0,0.0,40.0,30.0),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0.0,0.0,20,0.0),
+                child: Icon(Icons.calendar_today_outlined),
+              ),
+
+              Expanded(child: Text('Date Added')),
+              Expanded(
+                child: Text("11th June"),
+              )
+            ],
+          ),
+        ),
+          Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              new RaisedButton(
+             child: new Text("Commit Change"),
+             onPressed: (){},
+             color: kBlue,
+             textColor: Colors.white,
+             ),
+             Container(height: 20.0),//SizedBox(height: 20.0),
+             ],
+        ), 
+      ],
+    ),
+  );
 }
+
+
